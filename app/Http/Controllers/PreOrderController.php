f1241ccf16f260d308776ff1d1bf9104f0118ab4
phumiwit <68888298+phumiwit@users.noreply.github.com>
2021-10-28 02:32:24 +0700

    Update PreOrderController.php

--- app/Http/Controllers/PreOrderController.php
+++ app/Http/Controllers/PreOrderController.php
@@ -4,7 +4,37 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class PreOrderController extends Controller
+class PreOrdercontroller extends Controller
 {
-    //
+     public function index () {
+        $data = Preorder::all();
+        return view('Preorder.Preorder' , compact('data')) ; 
+    }
+    public function addPreorder(Request $request){
+        $data = new Preorder ;
+        $data -> orderNumber = $request -> orderNumber ;
+        $data -> productCode = $request -> productCode ;
+        $data -> preSale = $request -> preSale ;
+        $data -> quantity = $request -> quantity ;
+        $data -> save() ;
+        return redirect() -> back() ;
+}
+public function editPreorder($id){
+        $data = Preorder::find($id) ; 
+        return view('Preorder.edit', compact('data'));
+} 
+
+public function updatePreorder(Request $request){
+    
+    $data = Preorder::all();
+    $payment= Preorder::find($request->id) ;
+    $payment -> orderNumber = $request -> orderNumber ; 
+    $payment ->productCode = $request -> productCode ; 
+    $payment -> preSale = $request -> preSale ;
+    $payment -> quantity = $request -> quantity ; 
+    $payment -> save() ;
+    return view ('/Preorder.Preorder' , compact('data')) ; 
+}
+}
+
 }
